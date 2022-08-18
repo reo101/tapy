78db12bbf8b28360b6ca6547a109bea639a9adec
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-08-19 00:06:12 +0300

    feat(backend): correctly use `DATABASE_URL`

--- common/src/db.rs
+++ common/src/db.rs
@@ -5,10 +5,10 @@ pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
     let database_url = crate::util::fs::database_path()
-        .expect("DATABASE_URL must be set")
-        .into_os_string()
-        .into_string()
-        .expect("Bruv, replace with unwrap()");
+        .map(std::path::PathBuf::into_os_string)
+        .map(std::ffi::OsString::into_string)
+        .and_then(std::result::Result::ok)
+        .expect("Error setting database path");
 
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
--- common/src/util/fs.rs
+++ common/src/util/fs.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use directories_next::ProjectDirs;
+use dotenv::dotenv;
 
 fn data_path() -> Option<PathBuf> {
     let path = ProjectDirs::from("bg", "reo101", "Tapy")?.data_dir().to_path_buf();
@@ -9,9 +10,16 @@ fn data_path() -> Option<PathBuf> {
 }
 
 pub fn database_path() -> Option<PathBuf> {
-    let mut path = data_path()?;
-    path.push("db.db");
-    Some(path)
+    dotenv().ok();
+
+    match std::env::var("DATABASE_URL") {
+        Result::Ok(url) => Some(PathBuf::from(url)),
+        Result::Err(_) => {
+            let mut path = data_path()?;
+            path.push("db.db");
+            Some(path)
+        }
+    }
 }
 
 pub fn media_path() -> Option<PathBuf> {
