0ba055ee3dcb43d122b2d3b681e4b943c6395986
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-08-18 21:30:45 +0300

    feat(frontend): add unfinished Gallery to homepage

--- frontend/src/components/home.rs
+++ frontend/src/components/home.rs
@@ -6,18 +6,12 @@ use yew_router::prelude::*;
 pub fn home() -> Html {
     let history = use_history().unwrap();
 
-    let go_add = Callback::from(move |_| history.push(crate::Route::Add));
-    // let go_items = Callback::from(move |_| {
-    //     history.push(crate::Route::ItemsByTags {
-    //         tags: "".to_string(),
-    //     })
-    // });
+    let go_see = Callback::from(move |_| history.push(crate::Route::Gallery));
 
     html! {
         <div>
-            <h1>{ "Home pog" }</h1>
-            <button onclick={go_add}>{ "Go Add" }</button>
+            <h1>{ "Home" }</h1>
+            <button onclick={go_see}>{ "Go See" }</button>
         </div>
     }
-    // <button onclick={go_items}>{ "Go Items" }</button>
 }
